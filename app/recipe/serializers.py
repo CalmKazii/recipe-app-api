6ec7c99d530b6e2ddda1486ee9cfe8b51f71a80b
Sangima <sangima.soie@gmail.com>
2021-08-03 18:39:44 +0530

    added hustke recipe detail feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,5 +36,11 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ('id', 'title', 'ingredients','tags', 'time_minutes',
                   'price', 'link'
-        )
+                  )
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
